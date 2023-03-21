e23fb596c760822e7f384f24c333de1851693ca7
Mohamed Badawy <grmbadawy@gmail.com>
2023-03-21 20:05:09 +0200

    Connecting to database

---
+++ db.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const {MongoClient} = require('mongodb')
+
+
+const client = new MongoClient(process.env.CCONNECTIONSTRING)
+
+async function start() {
+   await client.connect()
+   module.exports = client.db()
+   const app = require('./app')
+   app.listen(process.env.PORT)
+}
+
+start()
